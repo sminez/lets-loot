b8c96dc3ad53db12557b2f0199ceca64988b8b05
sminez <innes.morrison@cocoon.life>
2017-04-13 13:52:18 +0100

    implementing basic attack logic

--- src/npcs/npc.py
+++ src/npcs/npc.py
@@ -45,19 +45,20 @@ class NPC:
         self.SYM = sym
         self.PACK = []
         self.NAME = name
-        self._atk_range = 0  # 0/1/2 -> close/medium/long
+        self.atk_range = 1  # 1/2/3 -> close/medium/long
         self.EQUIPMENT = {
             k: None for k in
             ['head', 'chest', 'larm', 'rarm', 'legs', 'feet', 'special']
         }
+        self.x = 0
+        self.y = 0
 
-    @property
-    def stats(self):
+    def __repr__(self):
         '''Pretty print this NPCs stats'''
         extra = ['NAME', 'SYM', 'MAX_HP']
         d = self.__dict__
         stats = ('{}: {}'.format(stat, d[stat]) for stat in extra + CORE_STATS)
-        print('CLASS: {}\n'.format(self.CLASS) + '\n'.join(stats))
+        return 'CLASS: {}\n'.format(self.CLASS) + '\n'.join(stats)
 
     def _randomise_core_stats(self, spread):
         '''Randomise the default stats for a character class.'''
@@ -71,11 +72,55 @@ class NPC:
         The target must be inside of the attack range.
         Attacker's bravery and sanity may affect the target as well.
         '''
-        if self._target_in_range(target):
+        if self.distance_to(target) <= self.atk_range:
             outcome = target.get_hit(self.ATK, self.BRV, self.SAN)
-            self._process_attack_outcome(outcome)
+            self.process_attack_outcome(outcome)
         else:
             raise FailedAttack('Out of range')
 
-    def _target_in_range(self, target):
-        return get_range(self, target) <= self._atk_range
+    def distance_to(self, other):
+        '''Integer distance to another object (rounded down)'''
+        return int(((self.x - other.x)**2 + (self.y - other.y)**2)**0.5)
+
+    def get_hit(self, atk, brv, san):
+        '''Take a hit from an attack'''
+        damage = atk - self.DEF
+        outcome = {'damage': damage, 'stat_based': {}}
+
+        if damage > 0:
+            self.HP -= damage
+            if self.HP <= 0:
+                self.die()
+
+        if san <= 20 and self.SAN > 20:
+            # Crazy people freak out normal people
+            reduction = ((self.SAN - san) // 10) + 1
+            self.BRV -= reduction
+            outcome['stat_based']['BRV'] = -reduction
+        elif brv >= 55 and self.SAN < 20:
+            # Heroically brave people beat insanity out you
+            increase = ((brv - self.SAN) // 10) + 1
+            self.SAN += increase
+            outcome['stat_based']['SAN'] = increase
+
+        return self.effects_on_attacked(outcome)
+
+    def effects_on_attacked(self, outcome):
+        '''For subclasses to add additional effects'''
+        return outcome
+
+    def process_attack_outcome(self, outcome):
+        '''Deal with post-attack effects on the attacker'''
+        if outcome['damage'] <= 0:
+            # Failed attacks demoralise
+            self.BRV -= 1
+        if outcome['damage'] <= -10:
+            # That REALLY failed
+            self.BRV -= 2
+
+        for k, v in outcome['stat_based'].items():
+            self.__dict__[k] += v
+
+    def die(self):
+        '''Handle character death effects'''
+        print('{} died...'.format(self.NAME))
